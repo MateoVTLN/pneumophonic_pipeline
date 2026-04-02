3669b3f28faa35fa05428a4eab149e2e5d8f9525
MateoVTLN <mateovtln@gmail.com>
2026-04-02 17:40:38 +0200

    Update run_quick_test.py

--- run_quick_test.py
+++ run_quick_test.py
@@ -5,8 +5,8 @@ results = run_pipeline(
     data_root="C:/Users/Matéo/OneDrive/Documents/GitHub/pneumophonic_pipeline/data_root/healthy_subjects",   
     output_root="C:/Users/Matéo/OneDrive/Documents/GitHub/pneumophonic_pipeline/data_target",
     tasks=['vowel', 'trill', 'glide'] 
-print(f"Analyzed {results.n_subjects} subjects")
     )
+print(f"Analyzed {results.n_subjects} subjects")
 """
 from pneumophonic_analysis import run_pipeline
 import logging
