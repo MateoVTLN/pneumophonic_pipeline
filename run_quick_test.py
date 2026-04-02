05cc95cc7151870d1622f7318209fa74d1407ed5
MateoVTLN <mateovtln@gmail.com>
2026-04-02 17:30:09 +0200

    Update run_quick_test.py

--- run_quick_test.py
+++ run_quick_test.py
@@ -1,18 +1,20 @@
 from pneumophonic_analysis import run_pipeline
 import logging
 logging.basicConfig(level=logging.DEBUG)
-# Run the complete analysis with automatic export
 results = run_pipeline(
-<<<<<<< Updated upstream
+    data_root="C:/Users/Matéo/OneDrive/Documents/GitHub/pneumophonic_pipeline/data_root/healthy_subjects",   
+    output_root="C:/Users/Matéo/OneDrive/Documents/GitHub/pneumophonic_pipeline/data_target",
+    tasks=['vowel', 'trill', 'glide'] 
+print(f"Analyzed {results.n_subjects} subjects")
+    )
+"""
+from pneumophonic_analysis import run_pipeline
+import logging
+logging.basicConfig(level=logging.DEBUG)
+results = run_pipeline(
     data_root="C:/Users/Matéo/OneDrive/Documents/GitHub/pneumophonic_pipeline/data_root",    
     output_root="C:/Users/Matéo/OneDrive/Documents/GitHub/pneumophonic_pipeline/data_target", 
     tasks=['vowel', 'trill', 'glide'] 
-=======
-    data_root="C:/Users/Matéo/OneDrive/Documents/GitHub/pneumophonic_pipeline/data_root/healthy_subjects",    # <-- UPDATE THIS PATH
-    output_root="C:/Users/Matéo/OneDrive/Documents/GitHub/pneumophonic_pipeline/data_target", # <-- UPDATE THIS PATH
-    tasks=['vowel', 'trill', 'glide'] # You can change these based on what you want to analyze
->>>>>>> Stashed changes
-)
-
 print(f"Analyzed {results.n_subjects} subjects")
-print(f"Successes: {results.n_successful}, Failures: {results.n_failed}")
+)
+"""
